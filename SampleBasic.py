2ae89d66df9106d4826485c8061e3505e6ff6613
NikolayIT <tkachenni@mail.ru>
2021-07-14 00:09:17 +0300

    update 04

--- SampleBasic.py
+++ SampleBasic.py
@@ -5,4 +5,20 @@ cv.imshow('Musk ', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', gray)
+
+#blur
+blur = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+cv.imshow('blur', blur)
+
+#Edge cascade
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny', canny)
+
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny blur', canny)
+
+#Dilating the image
+dilated = cv.dilate(canny, (3, 3), iterations=1)
+cv.imshow('dilated', dilated)
+
 cv.waitKey(0)
