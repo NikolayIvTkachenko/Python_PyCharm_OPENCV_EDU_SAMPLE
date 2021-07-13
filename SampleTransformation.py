2e4b708b650990271aaf3f86d11c8c6730f032ba
NikolayIT <tkachenni@mail.ru>
2021-07-14 01:11:15 +0300

    update 06

--- SampleTransformation.py
+++ SampleTransformation.py
@@ -18,4 +18,18 @@ def translate(img, x, y):
 translate = translate(img, 100, 100)
 cv.imshow('translate', translate)
 
+#Rotation
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated01 = rotate(img, 45)
+cv.imshow('rotated01', rotated01)
+
 cv.waitKey(0)
