b84282d838573ea67709e2d3acf1c64f08f8d695
NikolayIT <tkachenni@mail.ru>
2021-07-14 04:24:12 +0300

    update 13

--- SampleFaceDetection.py
+++ SampleFaceDetection.py
@@ -3,14 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = cv.imread('PhotoEdu/ElonMusk.jpg')
-cv.imshow('Elon Musk', img)
+#cv.imshow('Elon Musk', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('gray', gray)
+#cv.imshow('gray', gray)
 
 haar_cascade = cv.CascadeClassifier('haar/haarcascade_profileface.xml')
 faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
 print(f'Number of faces = {len(faces_rect)}')
+
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(img, (x,y),(x+w, y+h), (0, 255, 255), thickness=5)
+cv.imshow('Detective faces', img)
 
 cv.waitKey(0)
