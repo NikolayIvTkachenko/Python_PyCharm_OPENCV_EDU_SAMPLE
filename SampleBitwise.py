6c97a1162c94002158c3cb78c2ce147fc95e2003
NikolayIT <tkachenni@mail.ru>
2021-07-14 03:28:01 +0300

    update 09

--- SampleBitwise.py
+++ SampleBitwise.py
@@ -18,5 +18,12 @@ cv.imshow('bitwise_and ', bitwise_and )
 bitwise_or = cv.bitwise_or(rectangle, circle)
 cv.imshow('bitwise_or ', bitwise_or )
 
+#bitwise XOR
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+cv.imshow('bitwise_xor ', bitwise_xor )
+
+#bitwise NOT
+bitwise_not = cv.bitwise_not(rectangle, circle)
+cv.imshow('bitwise_not ', bitwise_not )
 
 cv.waitKey(0)
