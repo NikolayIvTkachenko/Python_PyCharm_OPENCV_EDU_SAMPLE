ce9a8fab48a85042d8ade8465e2b6e9a404daed6
NikolayIT <tkachenni@mail.ru>
2021-07-14 04:19:20 +0300

    update 12

--- SampleFaceDetection.py
+++ SampleFaceDetection.py
@@ -8,7 +8,9 @@ cv.imshow('Elon Musk', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', gray)
 
-haar_cascade = cv.CascadeClassifier('haar_face.xml')
+haar_cascade = cv.CascadeClassifier('haar/haarcascade_profileface.xml')
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
+print(f'Number of faces = {len(faces_rect)}')
 
 cv.waitKey(0)
--- SampleGradients.py
+++ SampleGradients.py
@@ -11,8 +11,14 @@ cv.imshow('gray', gray)
 #Laplacian
 lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
+cv.imshow('lap', lap)
 
+#sobel
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
 
+cv.imshow('SobelX', sobelx)
+cv.imshow('SobelY', sobely)
 
 cv.waitKey(0)
 
