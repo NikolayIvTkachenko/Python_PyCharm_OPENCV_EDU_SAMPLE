07c19a5c16ec09d0029dd26e04edcda781212a4b
NikolayIT <tkachenni@mail.ru>
2021-07-16 22:39:59 +0300

    project scan update

--- SampleProjectScanner.py
+++ SampleProjectScanner.py
@@ -1,3 +1,4 @@
+import cv2
 import cv2 as cv
 import numpy as np
 
@@ -15,21 +16,71 @@ def preProcessing(img):
     imgGray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
     imgBlur = cv.GaussianBlur(imgGray, (5, 5), 1)
     imgCanny = cv.Canny(imgBlur, 200, 200)
-    kernel = np.ones(5, 5)
+    kernel = np.ones((5, 5))
     imgDial = cv.dilate(imgCanny, kernel, iterations=2)
     imgThres = cv.erode(imgDial, kernel, iterations=1)
 
     return imgThres
 
+def getCounters(img):
+    biggest = np.array([])
+    maxArea = 0
+    counters, hierarchy = cv.findContours(img,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    for cnt in counters:
+        area = cv.contourArea(cnt)
+        #print(area)
+        if area > 5000:
+            cv.drawContours(imgCounters, cnt, -1, (255, 0, 0), 3)
+            peri = cv.arcLength(cnt, True)
+            approx = cv.approxPolyDP(cnt, 0.02 * peri, True)
+            print(approx)
+            if area > maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
 
+            #obj = len(approx)
+            #x, y, w, h = cv.boundingRect(approx)
+    cv.drawContours(imgCounters, biggest, -1, (255, 0, 0), 20)
+    return biggest
 
+def getWarp(img, biggest):
+    biggest = reorder(biggest)
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0,0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    matrix = cv.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    return imgOutput
+
+def reorder(points):
+    points = points.reshape((4,2))
+    pointsNew = np.zeros((4,1,2), np.int32)
+    add = points.sum(1)
+    print("add", add)
+
+    pointsNew[0] = points[np.argmin(add)]
+    pointsNew[3] = points[np.argmax(add)]
+
+    diff = np.diff(points, axis=1)
+    pointsNew[1] = points[np.argmin(diff)]
+    pointsNew[2] = points[np.argmax(diff)]
+
+    return pointsNew
 
 while True:
     success, img = cap.read()
     cv.resize(img, (widthImg, heightImg))
-    
-    imgThres = preProcessing(img)
+    imgCounters = img.copy()
 
-    cv.imshow("Result", img)
+    imgThres = preProcessing(img)
+    biggest = getCounters(imgThres)
+
+    print(biggest)
+
+    if len(biggest)>0:
+        imgWraped = getWarp(img, biggest)
+
+
+    cv.imshow("Result", imgCounters)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
